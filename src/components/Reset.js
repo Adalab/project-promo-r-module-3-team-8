ff23904b185ff0b8daf4f56615da13e1d07a5664
Yara <yara.gordillo@gmail.com>
2022-12-02 14:01:03 +0100

    downloads

--- src/components/Reset.js
+++ src/components/Reset.js
@@ -1,12 +1,13 @@
 function Reset(props) {
   return (
     <button
-    className='reset js-reset-btn'
-    type='reset'
-    onClick={props.handleReset}>
-    <p>Reset</p>
-    <i className='fa-regular fa-trash-can'></i>
-  </button>
-)
+      className='reset js-reset-btn'
+      type='reset'
+      onClick={props.handleReset}
+    >
+      <p>Reset</p>
+      <i className='fa-regular fa-trash-can'></i>
+    </button>
+  );
 }
 export default Reset;
